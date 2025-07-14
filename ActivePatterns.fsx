08d498b82b4b0132e21dd344168734fd5272776f
Walt <speewa@edc.ca>
2025-07-14 09:46:02 -0400

    ✨ active patterns

--- ActivePatterns.fsx
+++ ActivePatterns.fsx
@@ -12,3 +12,13 @@ TestNumber 7
 TestNumber 32
 TestNumber -1
 TestNumber -0
+
+// Currency
+open System
+
+let (|Currency|) (x:float) = 
+    Math.Round(x, 2)
+
+match 100.0 / 3.0 with
+| Currency 33.33 -> true
+| _ -> false
