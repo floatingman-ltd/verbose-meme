b3f94c1453f3be898c9154aa84f2d05b7e1b189d
Walt <speewa@edc.ca>
2025-07-16 19:41:02 -0400

    🚧 euler #2

--- Collections_Exercise_2.fsx
+++ Collections_Exercise_2.fsx
@@ -2,13 +2,14 @@
 let clip max collection =
   collection |> Seq.map (fun f -> min f max)
 
+// seq [1.0; 2.3; 10.0; -5.0]
 let clipped = [| 1.0; 2.3; 11.1; -5.0 |] |> clip 10.
 
 printfn "%A" clipped
 
 // min-max
 let min_max collection =
-  collection |> Array.min, //* note the coma and resultant implied tuple return with out parenthesis
+  collection |> Array.min,  //* note the coma and resultant implied tuple return with out parenthesis
   collection |> Array.max
 
 [| 1.0; 2.3; 11.1; -5.0 |] |> min_max
