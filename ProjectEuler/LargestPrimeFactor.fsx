8170b3347273cd2afe29ebba54649ca6baddc8c1
Walt <speewa@edc.ca>
2025-07-22 18:43:37 -0400

    🚧 euler #3 prime factors

--- ProjectEuler/LargestPrimeFactor.fsx
+++ ProjectEuler/LargestPrimeFactor.fsx
@@ -8,38 +8,42 @@ open System.Collections.Generic
 let testInt = 600_851_475_143L
 let testFloat = float testInt
 let largestPossibleFloat n = Math.Floor (Math.Sqrt n)
-let largestPossibleInt n = int (largestPossibleFloat n)
+let largestPossibleInt n = int64 (largestPossibleFloat n)
 let largestPossibleFactor (n : int64) = largestPossibleInt (float n)
 
 // get all the primes less then `largestPossibleInt`
 
-let primes = new List<_> ()
-primes.Add (2) |> ignore
 
-let isPrime f (primes : List<int>) =
-  // test if it is larger then the current largest known prime
-  // printfn "test:%i  last prime:%i" f (primes.Last ())
-  if f < primes.Last () then
-    false
-  elif f = primes.Last () then
-    true
-  else
-    let upperLimit = largestPossibleFactor f
-    // printfn "upper limit %i" upperLimit
-    let mutable prime = true
-    let mutable index = 0
+// trial division
 
-    while index < primes.Count && upperLimit >= primes[index] && prime do
-      // printfn "%i %% %i = %i" f primes[index] (f % primes[index])
-      if f % primes[index] = 0 then
-        prime <- false
-      else
-        index <- index + 1
+let primesToByTrial n =
+  // memoization
+  let primes = new List<_> ()
+  primes.Add 2L |> ignore
 
-    if prime then
-      primes.Add (f)
+  let rec isPrime n' =
+    let mutable isPrimeCandidate = true
+    let upperLimit = largestPossibleFactor n'
 
-    prime
+    match n' with
+    | n' when n' < primes.Last () -> false
+    | n' when n' = primes.Last () -> true
+    | _ ->
+      let mutable i = 0
+
+      while i < primes.Count && upperLimit >= primes[i] && isPrimeCandidate do
+        if n' % primes[i] = 0 then
+          isPrimeCandidate <- false
+        else
+          i <- i + 1
+
+      if isPrimeCandidate then
+        primes.Add n' |> ignore
+
+      isPrimeCandidate
+
+  // isPrime n
+
 
 //     if memo.ContainsKey x then
 
@@ -73,7 +77,33 @@ let isPrime f (primes : List<int>) =
 let sqrt = largestPossibleFactor testInt
 
 let p =
-  [ 2..sqrt ]
-  |> Seq.filter (fun i -> primes |> isPrime i)
-  |> Seq.filter (fun p -> testInt % (int64 p) = 0)
-  |> Seq.max 
+  [ 2L ] @ [ 3L .. 2L .. sqrt ]
+  |> Seq.filter (fun i -> primesToByTrial i)
+  |> Seq.filter (fun p -> testInt % int64 p = 0)
+  |> Seq.max
+// // 6857
+
+// let p = [ 3..2..sqrt ] |> Seq.filter (fun i -> primes |> isPrime i)
+
+// printfn "%A" p
+
+let primesTo n =
+  let rec sieve l =
+    match l with
+    | [] -> []
+    // this will match all lists as <head> :: <rest>, think of LISP car & cdr
+    | p :: xs ->
+      // this will join `p` to the front of the list returned by calling the `sieve` function
+      p
+      :: sieve
+        // the comprehension, where `xs` is the decomposed tail
+        [
+          for x in xs do
+            if (x % p) > 0 then
+              yield x
+        ]
+
+  [ 2 ] @ [ 3..2..n ] |> sieve
+
+// let answer =
+//   primesTo sqrt |> Seq.filter (fun p -> testInt % (int64 p) = 0) |> Seq.max
